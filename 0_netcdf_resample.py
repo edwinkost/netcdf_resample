80eef7d82221a3179b1ac7367de65ecff4e63c8a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-06-10 00:15:27 +0200

    Finalize 30min - precipitation.

--- 0_netcdf_resample.py
+++ 0_netcdf_resample.py
@@ -51,7 +51,7 @@ input_netcdf['cell_area']        = "/scratch/edwin/input/forcing/hyperhydro_wg1/
 output_netcdf = {}
 # cell size/length/resolution (arc-degree) for the output netcdf file 
 output_netcdf['cell_resolution'] = 30./60.
-output_netcdf['folder']          = "/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/netcdf_latlon/30min/precipitation/"
+output_netcdf['folder']          = "/scratch/edwin/input/forcing/hyperhydro_wg1/EFAS/netcdf_latlon/30min/"+varDict.netcdf_short_name[efas_variable_name]+"/"
 output_netcdf['file_name']       = varDict.netcdf_short_name[efas_variable_name]+"_efas_rhine-meuse.nc"
 output_netcdf['file_name']       = output_netcdf['folder']+"/"+output_netcdf['file_name']
 output_netcdf['variable_name']   = input_netcdf['variable_name']
